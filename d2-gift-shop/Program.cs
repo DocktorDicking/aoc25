37a5e71517487d29b03fa851834277ab346767a2
Jim <jvw91@msn.com>
2026-01-27 21:21:43 +0100

    feat(gift-shop): add range logic with basic validation. Very much wip.

--- d2-gift-shop/Program.cs
+++ d2-gift-shop/Program.cs
@@ -1,2 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+static Range LoadRange(Range range)
+{
+    if (range.start < range.end)
+    {
+        Console.WriteLine("Invalid range");
+        return range;
+    }
+    
+    range.values = Enumerable.Range(range.start, range.end - range.start + 1).ToArray();
+    return range;
+}
+
+static int[] GetInvalidValues(Range range)
+{
+    if (range.values.Length == 0)
+    {
+        Console.WriteLine("Values not loaded yet");
+        return [];
+    }
+
+    var values = range.values;
+    foreach (var value in values)
+    {
+        //TODO
+        return [];
+    }
+
+    return [];
+}
+
+string range = "11-22";
+
+string[] rangeParts = range.Split('-');
+int start = int.Parse(rangeParts[0]);
+int end = int.Parse(rangeParts[1]);
+
+struct Range
+{
+    public int[] values { get; set; }
+    public int start { get; set; }
+    public int end { get; set; }
+
+    public Range(int start, int end)
+    {
+        this.start = start;
+        this.end = end;
+    }
+}
